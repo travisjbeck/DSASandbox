18249a2d6940229618dedeaf0fc7b1bfcc9c3f33
Travis Beck <travis@travisjbeck.com>
2024-11-18 11:41:49 -0700

    completed using only 1 array

--- C/credit/credit.c
+++ C/credit/credit.c
@@ -1,105 +1,118 @@
-
-#include <stdio.h>
+// Luhn's Algorithm Implementation
 #include <cs50.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 const int MAX_DIGITS = 16;
 const int MIN_DIGITS = 13;
+const char *VISA = "VISA";
+const char *INVALID = "INVALID";
+const char *AMEX = "AMEX";
+const char *MASTERCARD = "MASTERCARD";
 
 int *get_number_array(long number, int length);
 void print_array(int *array, int size);
 int num_size(long num);
-void unzip_array(int *input, int length, int *left, int *right);
-void multiply_array(int *arr, int length, int multiplier);
-
-typedef struct
-{
-  int *items;
-  int size;
-} Array;
+void multiply_eod(int *arr, int start);
+int sum_eod(int *arr, int start);
+const char *categorize_card(int nums[], int length);
 
 int main(void)
 {
   long number = 0;
+
+  // get valid input
   while (number <= 0)
   {
     number = get_long("Number: ");
   }
 
+  // how many digits did we get?
   int length = num_size(number);
 
   if (length > MAX_DIGITS || length < MIN_DIGITS)
   {
-    printf("INVALID\n");
-    return 1;
+    printf("%s\n", INVALID);
+    return 0;
   }
 
-  // put each digit of our number into an array so we can work with each digit separately
+  // put each digit of our number into an array
   int *nums = get_number_array(number, length);
 
+  // before the destory our orignal data, test basic validation
+
+  const char *CARD_TYPE = categorize_card(nums, length);
+
+  if (CARD_TYPE == INVALID)
+  {
+    printf("%s\n", INVALID);
+    free(nums);
+    return 0;
+  }
+
   // STEP 1:
-  // Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
+  // Multiply every other digit by 2, starting with the number’s second-to-last digit.
+  multiply_eod(nums, length - 2);
 
-  // Unzip our array into left and right.
-  // Which one is left and which is right and what size are they?
-  // If the length is even, then they're both sized length / 2
-  // if the length is odd, the they're sized (length / 2 + 1) and (length / 2)
-
-  int remainder = length % 2;
-  int evenLength = (length / 2) + remainder;
-  int oddLength = (length / 2);
-  int *even = malloc(evenLength * sizeof(int));
-  int *odd = malloc(oddLength * sizeof(int));
-
-  unzip_array(nums, length, even, odd);
-  // if the length is odd (length % 2 > 0), the odd unzipped array is the one to multiply
-  // if the length is even (length % 2 == 0) the even unzipped array is the one to multiply
-  // create our Arrays with sizes to use for the operations
-  Array *nums_to_mult = malloc(sizeof(Array));
-  nums_to_mult->items = remainder == 0 ? even : odd;
-  nums_to_mult->size = remainder == 0 ? evenLength : oddLength;
-
-  Array *nums_left = malloc(sizeof(Array));
-  nums_left->items = remainder == 0 ? odd : even;
-  nums_left->size = remainder == 0 ? oddLength : evenLength;
-
-  print_array(nums_to_mult->items, nums_to_mult->size);
-
-  multiply_array(nums_to_mult->items, nums_to_mult->size, 2);
-
-  print_array(nums_to_mult->items, nums_to_mult->size);
+  // get the sum of the sum of digits from each array
+  int sum = sum_eod(nums, length - 2) + sum_eod(nums, length - 1);
+  if (sum % 10 != 0)
+  {
+    printf("%s\n", INVALID);
+  }
+  else
+  {
+    printf("%s\n", CARD_TYPE);
+  }
   free(nums);
-
-  free(even);
-  free(odd);
-  return 1;
+  return 0;
 };
 
-// takes an input arry and two empty array pointers.
-// unzips the input array into the two pointer arrays
-void unzip_array(int *input, int length, int *even, int *odd)
+const char *categorize_card(int nums[], int length)
 {
-  int odd_index = 0;
-  int even_index = 0;
-  for (int i = 0; i < length; i++)
+  if (nums[0] == 4 && (length == 13 || length == 16))
   {
-    if (i % 2 == 0)
+    return VISA;
+  }
+
+  if (nums[0] == 5 && (nums[1] > 0 && nums[1] < 6) && length == 16)
+  {
+    return MASTERCARD;
+  }
+  if (nums[0] == 3 && (nums[1] == 4 || nums[1] == 7) && length == 15)
+  {
+    return AMEX;
+  }
+
+  return INVALID;
+}
+
+// adds each individual digit from an array of ints from right to left
+int sum_eod(int *arr, int start)
+{
+  int total = 0;
+  for (int i = start; i >= 0; i -= 2) // every other digit
+  {
+    if (arr[i] > 9)
     {
-      even[even_index] = input[i];
-      even_index++;
+      // break it into its two digits and add them
+      int *nums = get_number_array(arr[i], 2);
+      total += (nums[0] + nums[1]);
+      free(nums);
     }
     else
     {
-      odd[odd_index] = input[i];
-      odd_index++;
+      total += arr[i];
     }
   }
+  return total;
 }
 
-void multiply_array(int *arr, int length, int multiplier)
+// multiplies every other int in an array * 2 in place from right to left
+void multiply_eod(int *arr, int start)
 {
-  for (int i = 0; i < length; i++)
+  for (int i = start; i >= 0; i -= 2)
   {
     arr[i] = arr[i] * 2;
   }
@@ -137,7 +150,7 @@ void print_array(int *array, int size)
 {
   for (int i = 0; i < size; i++)
   {
-    printf("%i", array[i]);
+    printf("%i ", array[i]);
   }
   printf("\n");
 }
