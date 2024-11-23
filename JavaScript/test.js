fe8b4045a3c94dcea7318de7b698ccd55e0cdf17
Travis Beck <travis@travisjbeck.com>
2024-11-22 17:12:38 -0700

    image filtering (grayscale, mirror, blur, edge detection)

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,24 +1,8 @@
-// function isPrime(n) {
-//   for (var i = 2; i <= Math.floor(Math.log2(n)); i++) {
-//     if (n % i == 0) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
-// console.log(Math.floor(Math.log2(10)));
-
-
-function isPrime(n) {
-  for (var i = 1; i <= n; i++) {
-    if ((i !== 1) && (i !== n) && (n % i == 0)) {
-      return false;
-    }
-  }
-  return true;
+function getOffset(col, c) {
+  return Math.abs((col - c) - 1);
 }
 
 
+console.log(getOffset(1, 2));
 
-console.log(isPrime(0));
+
--- cs50/C/filter-more/helpers.c
+++ cs50/C/filter-more/helpers.c
@@ -1,10 +1,25 @@
 #include "helpers.h"
+#include <math.h>
+
+// set up our sobel kernels
+//------Gx-----
+//  -1   0    1
+//  -2   0    2
+//  -1   0    1
+
+//------Gy-----
+// -1  -2   -1
+//  0   0    0
+//  1   2    1
+const int GX[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+const int GY[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
 
 RGBTRIPLE get_3x3_average(int height, int width, RGBTRIPLE image[height][width], int row, int col);
 int min(int a, int b);
 void copy_array(int height, int width, RGBTRIPLE image[height][width],
                 RGBTRIPLE copy[height][width]);
 int max(int a, int b);
+RGBTRIPLE get_edge_value(int height, int width, RGBTRIPLE image[height][width], int row, int col);
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -18,7 +33,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             int green = image[i][j].rgbtGreen;
             int blue = image[i][j].rgbtBlue;
 
-            int avg = (blue + red + green) / 3;
+            double sum = (red + blue + green);
+            int avg = round(sum / 3);
 
             image[i][j].rgbtRed = avg;
             image[i][j].rgbtGreen = avg;
@@ -72,30 +88,35 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+
+    // copy the array so we don't average already averaged pixels
+    copy_array(height, width, image, copy);
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = get_edge_value(height, width, copy, i, j);
+        }
+    }
+
     return;
 }
 
 RGBTRIPLE get_3x3_average(int height, int width, RGBTRIPLE image[height][width], int row, int col)
 {
-    // how to get neighbors?
+    // Blur an image by averaging it with its neighbors
 
-    //   [row-1][col-1]              [row-1][col]            [row-1][col +1]
-    //      topLeft                    top                      topRight
-    //
-    //   [row][col-1]                                          [row][col +1]
-    //      left                       self                        right
-    //
-    //  [row + 1][col - 1]         [row +1][col]            [row +1][col +1]
-    //    bottomLeft                  bottom                 bottomRight
     int prev_row = max(0, row - 1);
     int prev_col = max(0, col - 1);
     int next_row = min(height - 1, row + 1);
     int next_col = min(width - 1, col + 1);
 
-    int sum_blue = 0;
-    int sum_green = 0;
-    int sum_red = 0;
-    int total_values = 0;
+    double sum_blue = 0;
+    double sum_green = 0;
+    double sum_red = 0;
+    double total_values = 0;
 
     for (int r = prev_row; r <= next_row; r++)
     {
@@ -108,11 +129,73 @@ RGBTRIPLE get_3x3_average(int height, int width, RGBTRIPLE image[height][width],
         }
     }
 
-    sum_blue = sum_blue / total_values;
-    sum_green = sum_green / total_values;
-    sum_red = sum_red / total_values;
+    sum_blue = round(sum_blue / total_values);
+    sum_green = round(sum_green / total_values);
+    sum_red = round(sum_red / total_values);
     RGBTRIPLE avg = {sum_blue, sum_green, sum_red};
     return avg;
+}
+
+RGBTRIPLE get_sobel_pixel_value(int height, int width, RGBTRIPLE image[height][width], int row, int col)
+{
+    RGBTRIPLE blk_px = {0, 0, 0};
+    if (row < 0 || row >= height)
+    {
+        return blk_px;
+    }
+    if (col < 0 || col >= width)
+    {
+        return blk_px;
+    }
+
+    return image[row][col];
+}
+
+RGBTRIPLE get_edge_value(int height, int width, RGBTRIPLE image[height][width], int row, int col)
+
+{
+    double sum_blue_x = 0;
+    double sum_green_x = 0;
+    double sum_red_x = 0;
+
+    double sum_blue_y = 0;
+    double sum_green_y = 0;
+    double sum_red_y = 0;
+
+    int kernel = 0;
+
+    for (int r = row - 1; r <= row + 1; r++)
+    {
+        for (int c = col - 1; c <= col + 1; c++)
+        {
+
+            int offset_c = c - col + 1;
+            int offset_r = r - row + 1;
+
+            kernel = GX[offset_r][offset_c];
+
+            RGBTRIPLE pixel = get_sobel_pixel_value(height, width, image, r, c);
+
+            sum_blue_x += pixel.rgbtBlue * kernel;   // * Gx[][]
+            sum_green_x += pixel.rgbtGreen * kernel; // * Gx[][]
+            sum_red_x += pixel.rgbtRed * kernel;     // * Gx[][]
+
+            kernel = GY[offset_r][offset_c];
+
+            sum_blue_y += pixel.rgbtBlue * kernel;   // * Gy[][]
+            sum_green_y += pixel.rgbtGreen * kernel; // * Gy[][]
+            sum_red_y += pixel.rgbtRed * kernel;     // * Gy[][]
+        }
+    }
+    // The Sobel filter algorithm combines Gx and Gy into a final value by calculating the square
+    // root of Gx^2 + Gy^2 rounded and set to 255 max (F) since we're dealing with hex color values
+
+    double gxy_red = min(255, round(sqrt((pow(sum_red_x, 2) + pow(sum_red_y, 2)))));
+    double gxy_blue = min(255, round(sqrt((pow(sum_blue_x, 2) + pow(sum_blue_y, 2)))));
+    double gxy_green = min(255, round(sqrt((pow(sum_green_x, 2) + pow(sum_green_y, 2)))));
+
+    RGBTRIPLE edge = {gxy_blue, gxy_green, gxy_red};
+    return edge;
 }
 
 int max(int a, int b)
@@ -132,7 +215,7 @@ int min(int a, int b)
     }
     return b;
 }
-
+//
 void copy_array(int height, int width, RGBTRIPLE image[height][width],
                 RGBTRIPLE copy[height][width])
 {
